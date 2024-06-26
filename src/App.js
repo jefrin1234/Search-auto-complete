03303ac12f84e948f683e5626ded1c413e4bee31
jefrin james <jamesjefrin13@gmail.com>
2024-06-26 09:36:31 +0530

    added all files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchAutoComplete from './search-autocomplete-with-api';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchAutoComplete/>
+      
+    </div>
+  );
+}
+
+export default App;
